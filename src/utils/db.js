895bd1e771009e7dce5d68ac78873d7934bb63a8
juan cruz benitez arce <juancruzbeniteza@gmail.com>
2024-06-06 20:40:56 -0300

    Deploy 1

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,8 +4,13 @@ import logger from "./logger/index.js"
 
 const dbConnection = async () => {
  try {
-   await connect(env.LINK_MONGO)
-   logger.INFO("Database Connected");
+   await connect(env.LINK_MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000 // 45 seconds
+  });
+    logger.INFO("Database Connected");
  } catch (error) {
    logger.WARN;;(error.message);
  } 
